ac669d261b9aac5a1296e27001e254993c00ffc4
Your Name <you@example.com>
2023-01-23 23:57:40 +0700

    delete and update post

--- client/src/modules/profile/ProfileGeneral.js
+++ client/src/modules/profile/ProfileGeneral.js
@@ -2,27 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import CakeIcon from "@mui/icons-material/Cake";
-import TextLight from "components/text/TextLight";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import BusinessIcon from "@mui/icons-material/Business";
 import TextInfo from "components/text/TextInfo";
 
 const ProfileGeneral = ({ dateJoin = "" }) => {
   return (
-    <div className="grid grid-cols-2 mt-4 gap-y-2">
-      <TextInfo content={"Join " + dateJoin}>
-        <CalendarMonthIcon className="text-lg" />
-      </TextInfo>
-      <TextInfo content="Born on 25/11/2022">
-        <CakeIcon className="text-lg" />
-      </TextInfo>
-      <TextInfo content="200 Friends">
-        <PersonAddIcon className="text-lg" />
-      </TextInfo>
+    <>
+      <div className="grid grid-cols-2 mt-4 gap-y-2">
+        <TextInfo content={"Join " + dateJoin}>
+          <CalendarMonthIcon className="text-lg" />
+        </TextInfo>
+        <TextInfo content="Born on 25/11/2022">
+          <CakeIcon className="text-lg" />
+        </TextInfo>
+        <TextInfo content="200 Friends">
+          <PersonAddIcon className="text-lg" />
+        </TextInfo>
+        <TextInfo content="Work at ...">
+          <BusinessIcon className="text-lg" />
+        </TextInfo>
+      </div>
       <TextInfo content="Work at ...">
         <BusinessIcon className="text-lg" />
       </TextInfo>
-    </div>
+    </>
   );
 };
 
